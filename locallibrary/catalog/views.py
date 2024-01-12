29328b2321c4493aefd7fdd81dd807b53c9692aa
oleg <shishatskiy@mail.ru>
2024-01-12 11:48:41 +0700

    virtualenv and pip reset made

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -40,11 +40,12 @@ def index(request):
     )
 
 
-@login_required
-@permission_required('catalog.can_mark_returned', raise_exception=True)
+@permission_required('catalog.can_mark_returned')
 def renew_book_librarian(request, pk):
-    """View function for renewing a specific BookInstance by librarian."""
-    book_instance = get_object_or_404(BookInstance, pk=pk)
+    """
+    View function for renewing a specific BookInstance by librarian
+    """
+    book_inst = get_object_or_404(BookInstance, pk=pk)
 
     # If this is a POST request then process the Form data
     if request.method == 'POST':
@@ -55,21 +56,18 @@ def renew_book_librarian(request, pk):
         # Check if the form is valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
-            book_instance.due_back = form.cleaned_data['renewal_date']
-            book_instance.save()
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.save()
 
             # redirect to a new URL:
-            return HttpResponseRedirect(reverse('all-borrowed'))
+            return HttpResponseRedirect(reverse('all-borrowed') )
 
-    # If this is a GET (or any other method) create the default form
+    # If this is a GET (or any other method) create the default form.
     else:
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
 
-    context = {
-        'form': form,
-        'book_instance': book_instance,
-    }
+    return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
 
 
 class BookListView(LoginRequiredMixin, generic.ListView):
